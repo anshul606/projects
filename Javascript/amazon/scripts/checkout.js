3d2019fa93e9d0845bd7b769030b4371e1e1a463
Anshul <Anshulbansal2406@gmail.com>
2025-07-03 20:56:25 +0530

    Integrate updateDeliveryOption function and enhance delivery option selection in checkout process

--- Javascript/amazon/scripts/checkout.js
+++ Javascript/amazon/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
@@ -15,21 +15,21 @@ cart.forEach((cartItem) => {
     if (product.id === productId) {
       matchingProduct = product;
     }
-  });
-
-  const deliveryOptionId = cartItem.deliveryOptionId;
-  
-  let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
     });
 
-    const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays, `day`)
-    const dateString = deliveryDate.format(`dddd, MMM D`);
+    const deliveryOptionId = cartItem.deliveryOptionId;
+
+    let deliveryOption;
+        deliveryOptions.forEach((option) => {
+          if (option.id === deliveryOptionId) {
+            deliveryOption = option;
+          }
+        });
+
+        const today = dayjs();
+        const deliveryDate = today.add(deliveryOption.deliveryDays, `days`)
+        const dateString = deliveryDate.format(`dddd, MMM D`);
+    
 
   cartSummaryHTML += `
     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -80,7 +80,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
 
     deliveryOptions.forEach((deliveryOption) => {
         const today = dayjs();
-        const deliveryDate = today.add(deliveryOption.deliveryDays, `day`)
+        const deliveryDate = today.add(deliveryOption.deliveryDays, `days`)
         const dateString = deliveryDate.format(`dddd, MMM D`);
 
         const priceString = deliveryOption.priceCents === 0
@@ -90,7 +90,8 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
         html += `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option" 
+                data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
                   <input type="radio" ${isChecked ? `checked` : ``}
                     class="delivery-option-input"
                     name="delivery-option-${matchingProduct.id}">
@@ -117,4 +118,14 @@ document.querySelectorAll(`.js-delete-link`).forEach((link) => {
         const container = document.querySelector(`.js-cart-item-container-${productId}`)
         container.remove();
     });
+});
+
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element) => {
+      element.addEventListener('click', () => {
+        const {productId, deliveryOptionId} = element.dataset;
+
+
+        updateDeliveryOption(productId, deliveryOptionId);
+      });
 });
