3f4cdade12a95f3034db5f2d31b8c03a089b0636
Anshul <Anshulbansal2406@gmail.com>
2025-07-03 18:48:32 +0530

    Refactor checkout process: integrate removeFromCart function and enhance cart item deletion functionality

--- Javascript/amazon/scripts/checkout.js
+++ Javascript/amazon/scripts/checkout.js
@@ -1,22 +1,22 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = ``;
 
 cart.forEach((cartItem) => {
-    const productId = cartItem.productId;
+  const productId = cartItem.productId;
 
-    let matchingProduct;
+  let matchingProduct;
 
-    products.forEach((product) => {
-        if (product.id === productId) {
-            matchingProduct = product;
-        }
-    });
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
 
-    cartSummaryHTML += `
-    <div class="cart-item-container">
+  cartSummaryHTML += `
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -34,12 +34,14 @@ cart.forEach((cartItem) => {
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                    Quantity: <span class="quantity-label">${
+                      cartItem.quantity
+                    }</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -91,7 +93,16 @@ cart.forEach((cartItem) => {
               </div>
             </div>
           </div>
-    `
+    `;
+});
 
-    document.querySelector(`.js-order-summary`).innerHTML = cartSummaryHTML;
+document.querySelector(`.js-order-summary`).innerHTML = cartSummaryHTML;
+
+document.querySelectorAll(`.js-delete-link`).forEach((link) => {
+    link.addEventListener(`click`, () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+        const container = document.querySelector(`.js-cart-item-container-${productId}`)
+        container.remove();
+    });
 });
