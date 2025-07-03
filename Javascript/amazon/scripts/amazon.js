b978092d2c86ac5c6c9bdd9288a130867309007f
Anshul <anshul@anshuls-MacBook-Pro.local>
2025-07-03 13:50:06 +0530

    Amazon5

--- Javascript/amazon/scripts/amazon.js
+++ Javascript/amazon/scripts/amazon.js
@@ -1,3 +1,41 @@
+const products = [{
+    image: `images/products/athletic-cotton-socks-6-pairs.jpg`,
+    name: `Black and Gray Athletic Cotton Socks - 6 Pairs`,
+    rating: {
+        stars: 4.5,
+        reviews: 87
+    },
+    priceCents: 1090
+},
+{
+    image: `images/products/intermediate-composite-basketball.jpg`,
+    name: `Intermediate Size Basketball`,
+    rating: {
+        stars: 4.0,
+        reviews: 127
+    },
+    priceCents: 2095
+},
+{
+    image: `images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg`,
+    name: `Adults Plain Cotton T-Shirt - 2 Pack`,
+    rating: {
+        stars: 4.5,
+        reviews: 56
+    },
+    priceCents: 799
+},
+{
+    image: `images/products/black-2-slot-toaster.jpg`,
+    name: `2 slot Toaster - Black`,
+    rating: {  
+        stars: 5.0,
+        reviews: 2197
+    },
+    priceCents: 1899
+}];
+
+
 let productsHTML = ``;
 
 products.forEach((product) => {
@@ -16,7 +54,7 @@ products.forEach((product) => {
             <img class="product-rating-stars"
               src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.count}
+              ${product.rating.reviews}
             </div>
           </div>
 
